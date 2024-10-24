834b786ac574b314ac386ac0a431abfe09c1d199
Callum <callumdyson420@gmail.com>
2024-10-24 10:35:51 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿#region Program Outline
+    // Note: This program is being created to increase my knowledge in C#
+    /*
+     * This program will ask the user how much money they have to spend on a new computer
+     * The money entered will limit what PC components can be used
+     * PC components will be compatibility checked - this will be done with research (references will be mentioned)
+     * If all parts are compatible, a final list will be displayed with the full specs of the PC chosen
+     */ 
+#endregion
