66913060d08e81be55791dd87d8ac355332953e9
Callum <callumdyson420@gmail.com>
2024-10-24 10:58:22 +0100

    Created all the regions for the PC components and the region regarding the budget

--- Program.cs
+++ Program.cs
@@ -1,9 +1,74 @@
 ﻿#region Program Outline
-    // Note: This program is being created to increase my knowledge in C#
-    /*
-     * This program will ask the user how much money they have to spend on a new computer
-     * The money entered will limit what PC components can be used
-     * PC components will be compatibility checked - this will be done with research (references will be mentioned)
-     * If all parts are compatible, a final list will be displayed with the full specs of the PC chosen
-     */ 
+// Note: This program is being created to increase my knowledge in C#
+/*
+ * This program will ask the user how much money they have to spend on a new computer
+ * The money entered will limit what PC components can be used
+ * PC components will be compatibility checked - this will be done with research (references will be mentioned)
+ * If all parts are compatible, a final list will be displayed with the full specs of the PC chosen
+ * Note 2: At the moment, this will assume that only the PC is needed
+ */
+#endregion
+#region CPUs
+// This section will list the CPUs that can be chosen
+
+
+#endregion
+#region Memory
+// This section will list the memory that can be chosen and the configurations
+
+
+#endregion
+#region CPU Coolers
+/* 
+ * This section will list the CPU coolers that can be chosen
+ * The list can be filtered based on the CPU to ensure compatibility
+ */
+
+#endregion
+#region Motherboards
+/*
+ * This section will list all available motherboards to choose from
+ * The list will then be filtered based on the chosen CPU and memory configuration
+ */
+
+#endregion
+#region HDDs
+// This section will list all the available Hard Drives that are available
+
+
+#endregion
+#region SSDs
+// This section will list all the available Solid State Drives that are available
+
+
+#endregion
+#region M.2 Drives
+// This section will list all the available M.2 Drives that are available
+
+
+#endregion
+#region Graphics Cards
+/*
+ * This section will list all the available graphics cards that are available
+ * This will be filtered based on the remaining budget
+ */
+
+#endregion
+#region Power Supplies
+/*
+ * This section will list all the available Power Supplies that are available
+ * This will be filtered based on all the other PC components to ensure correct wattage
+ */
+
+#endregion
+#region PC Cases
+/*
+ * This section will list the PC cases that can be chosen from
+ * The list will then be filtered based on previous parts to ensure compatibility
+ */
+
+#endregion
+#region Budget
+// This section will allow input from the user and based on the amount, PC components will be listed!
+
 #endregion
